c4cff72561dcef8b0629553706b05c330c063176
Brighten Lee <brighten0522@gmail.com>
2021-09-12 01:18:41 +0900

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -119,14 +119,6 @@ fi
 
 
 # Install RealSense SDK
-if [ "$ROS_DISTRO" == "melodic" ]; then
-  sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDEfi
-  sudo add-apt-repository "deb http://librealsense.intel.com/Debian/apt-repo bionic main" -u
-  sudo apt install -y librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg
-fi
-
-if [ "$ROS_DISTRO" == "noetic" ]; then
-  sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDEfi
-  sudo add-apt-repository "deb http://librealsense.intel.com/Debian/apt-repo focal main" -u
-   sudo apt install -y librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg
-fi
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
+sudo apt install -y librealsense2-dkms librealsense2-utils
